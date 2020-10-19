197cdde7c0a57c146e67afebe792fdcbc8666217
19h47 <jeremylevron@19h47.fr>
2020-10-19 09:21:15 +0200

    :building_construction: Webpack 5 is coming

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,20 +1,15 @@
 /**
  *
- * @file webpack.common.js
+ * @file webpack.config.common.js
  * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
  */
-
-// Node modules
-const path = require('path');
 
 // Plugins
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 
-function resolve(dir) {
-	return path.join(__dirname, '..', dir);
-}
+const resolve = require('./webpack.utils');
 
 module.exports = {
 	entry: {
@@ -32,6 +27,13 @@ module.exports = {
 		port: 3000,
 		inline: true,
 		disableHostCheck: true,
+		writeToDisk: true,
+	},
+	resolve: {
+		alias: {
+			'@': resolve('src'),
+			Utils: resolve('src/utils'),
+		},
 	},
 	module: {
 		rules: [
@@ -50,12 +52,13 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin({
-			cleanOnceBeforeBuildPatterns: [resolve('docs')],
+			cleanOnceBeforeBuildPatterns: [resolve('dist'), resolve('docs')],
 		}),
 		new HtmlWebpackPlugin({
 			filename: resolve('docs/index.html'),
 			template: resolve('index.html'),
 			inject: false,
+			minify: { removeRedundantAttributes: false },
 		}),
 		new WebpackNotifierPlugin({
 			title: 'Webpack',
---
+++ webpack/webpack.config.production.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @file webpack.production.js
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ */
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+const resolve = require('./webpack.utils');
+
+module.exports = {
+	mode: 'production',
+	devtool: false,
+	watch: false,
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: resolve('clamp.png'), to: resolve('docs/') }],
+		}),
+	],
+};
